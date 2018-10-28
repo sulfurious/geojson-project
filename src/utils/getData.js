df379d859fab8c5ed6df84ef190546324470aa51
Timothy Eagan <tim@moduscreate.com>
2018-10-28 14:04:51 -0400

    add year range to stat properties

--- src/utils/getData.js
+++ src/utils/getData.js
@@ -1,7 +1,8 @@
 import geoJsonStats from 'geojson-stats'
 import crashData from 'resources/data.json'
 
-const statProperties = ['total', '2010', '2011', '2012', '2013', '2014']
+export const years = ['2010', '2011', '2012', '2013', '2014']
+const statProperties = ['total', ...years]
 
 const getStats = () => {
   const properties = ['length', 'area', ...statProperties]
@@ -12,7 +13,10 @@ const getStats = () => {
     [property]: {
       ...stats[idx]
     }
-  }), {})
+  }), {
+    minYear: parseInt(years[0], 10),
+    maxYear: parseInt(years[years.length - 1], 10)
+  })
 }
 
 export const dataCenter = [38.90364199734993, -77.02342987060545]
